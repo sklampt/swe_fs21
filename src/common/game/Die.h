2be733e59c1ae4058578abe8cd3d97c343e8960b
boosters <boosters@student.ethz.ch>
2021-05-19 06:39:16 +0000

    Resolve "Implement Class Cup"

--- src/common/game/Die.h
+++ src/common/game/Die.h
@@ -5,9 +5,27 @@
 #ifndef ZOMBIEDICE_DIE_H
 #define ZOMBIEDICE_DIE_H
 
+#include <random>
+
+enum Color {
+    green, yellow, red
+};
+enum Face {
+    brains, runners, shotgun
+};
 
 class Die {
+private:
+    Color _color;
+    Face _face;
+    std::mt19937 _gen;
+    std::uniform_int_distribution _dist;
+public:
+    Die(Color c);
 
+    Face getFace();
+
+    void roll();
 };
 
 
--- src/common/game/Turn.h
+++ src/common/game/Turn.h
@@ -5,9 +5,33 @@
 #ifndef ZOMBIEDICE_TURN_H
 #define ZOMBIEDICE_TURN_H
 
+#include "Cup.h"
+#include "Die.h"
+#include <algorithm>
+#include <vector>
+#include <iostream>
 
 class Turn {
+private:
+    std::vector<Die> _brains;
+    std::vector<Die> _footsteps;
+    std::vector<Die> _shotguns;
+    int _num_brains;
+    int _num_footprints;
+    int _num_shotguns;
+    Cup _cup;
 
+public:
+    Turn();
+
+    void roll();
+    int play_turn();
+
+    int getNumShotguns();
+
+    int getNumFootprints();
+
+    int getNumBrains();
 };
 
 
