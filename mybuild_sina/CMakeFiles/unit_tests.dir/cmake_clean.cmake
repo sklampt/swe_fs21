b523de492bee135528b7223233d79a57e553b813
Sina Klampt <sklampt@student.ethz.ch>
2021-05-23 23:33:30 +0200

    build folder

---
+++ mybuild_sina/CMakeFiles/unit_tests.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/unit_tests.dir/tests/example_add.cpp.o"
+  "CMakeFiles/unit_tests.dir/tests/main.cpp.o"
+  "unit_tests"
+  "unit_tests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/unit_tests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
