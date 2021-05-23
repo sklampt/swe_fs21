eac6ae00156df194017c2c16053a43579401f5df
Sina Klampt <sklampt@student.ethz.ch>
2021-05-23 23:32:59 +0200

    change layout to grid format

--- src/gui/panels/LobbyPanel.cpp
+++ src/gui/panels/LobbyPanel.cpp
@@ -10,39 +10,63 @@ LobbyPanel::LobbyPanel(wxWindow* parent) : wxPanel(parent, wxID_ANY) {
     wxColor white = wxColor(255, 255, 255);
     this->SetBackgroundColour(white);
 
-    wxBoxSizer* verticalLayout = new wxBoxSizer(wxVERTICAL);
+    //wxBoxSizer* verticalLayout = new wxBoxSizer(wxVERTICAL);
    
+    Create(parent);
+    SetClientSize(wxDefaultSize);
+    Move(wxDefaultPosition);
+    wxBoxSizer* verticalLayout = new wxBoxSizer(wxVERTICAL);
+    //Panel1 = new wxPanel(this, wxDefaultPosition, wxDefaultSize, wxTAB_TRAVERSAL);
+
     ImagePanel* logo = new ImagePanel(this, "assets/zombiedice_logo_nobackground.png", wxBITMAP_TYPE_ANY, wxDefaultPosition, wxSize(200, 250));
     verticalLayout->Add(logo, 0, wxALIGN_CENTER | wxTOP | wxLEFT | wxRIGHT, 10);
 
+    this->_display = new wxGrid(this, wxID_ANY, wxDefaultPosition, wxSize(400,300));
 
+/*
+    this->_display = new wxGrid(
+        this,
+        -1,
+        wxDefaultPosition,
+        wxSize(400, 300)
+    );
+ */   
+    _display->CreateGrid(2, 2);
+    _display->SetCellValue(0, 0, "You:");
+    _display->SetCellValue(0, 1, "blabla");
+    _display->SetCellValue(1, 0, "Other players:");
+    _display->SetCellValue(1, 1, "blabla:");
+    verticalLayout->Add(this->_display, 0, wxALIGN_CENTER | wxTOP | wxLEFT | wxRIGHT, 10);
+
+    /*
     this->_displayMeLHS = new wxStaticText(
             "You:",
             wxDefaultPosition,
-            wxSize(200, 18),
-            textAlignment,
-            true
+            wxSize(200, 18)
+            //wxAlignment,
+            //true
     );
     verticalLayout->Add(this->_displayMeLHS, 0, wxALIGN_CENTER | wxTOP | wxLEFT | wxRIGHT, 10);
 
     this->_displayMeRHS = new wxStaticText(
-            me->get_player_name(),
+            //me->get_player_name(),
             wxDefaultPosition,
-            wxSize(200, 18),
-            textAlignment,
-            true
+            wxSize(200, 18)
+            //wxAlignment,
+            //true
     );
     verticalLayout->Add(this->_displayMeLHS, 0, wxALIGN_CENTER | wxTOP | wxLEFT | wxRIGHT, 10);
 
     this->_displayOtherPlayersLHS = new wxStaticText(
             "Other players:",
             wxDefaultPosition,
-            wxSize(200, 18),
-            textAlignment,
-            true
+            wxSize(200, 18)
+            //wxAlignment,
+            //true
     );
     verticalLayout->Add(this->_displayMeLHS, 0, wxALIGN_CENTER | wxTOP | wxLEFT | wxRIGHT, 10);
 
+    
     for(int i = 0; i < players.size(); i++) {
         player* playerState = players.at(i);
         std::string playerName = playerState->get_player_name();
@@ -58,7 +82,7 @@ LobbyPanel::LobbyPanel(wxWindow* parent) : wxPanel(parent, wxID_ANY) {
             verticalLayout->Add(this->_displayMeLHS, 0, wxALIGN_CENTER | wxTOP | wxLEFT | wxRIGHT, 10);
         }
     }
-
+    */
 
     wxButton* connectButton = new wxButton(this, wxID_ANY, "Start", wxDefaultPosition, wxSize(100, 40));
     connectButton->Bind(wxEVT_BUTTON, [](wxCommandEvent& event) {
