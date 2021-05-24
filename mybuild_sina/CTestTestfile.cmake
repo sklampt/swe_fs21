8080c5f015f92519071ae1fbb86d444ce905a379
Sina Klampt <sklampt@student.ethz.ch>
2021-05-24 12:05:02 +0200

    delete mybuild_sina

--- mybuild_sina/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sklampt/eth/fs21/software_engineering/zombiedice
-# Build directory: /home/sklampt/eth/fs21/software_engineering/zombiedice/mybuild_sina
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- mybuild_sina/gtest_binary/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sklampt/eth/fs21/software_engineering/zombiedice/mybuild_sina/gtest_source/googlemock
-# Build directory: /home/sklampt/eth/fs21/software_engineering/zombiedice/mybuild_sina/gtest_binary/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
