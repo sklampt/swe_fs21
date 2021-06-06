b6781d59daa457b5e8539b75fbee3d18e5629430
Robin Worreby <rworreby@student.ethz.ch>
2021-06-06 14:02:35 +0200

    Add unit test for exception handling

---
+++ tests/common/exceptions.cpp
@@ -0,0 +1,9 @@
+#include "gtest/gtest.h"
+#include "common/exceptions/ZombieException.h"
+
+TEST(Exceptions, Zombie_Exception_Constructor)
+{
+    std::string test_message = "Test";
+    ZombieDiceException exception(test_message);
+    ASSERT_STREQ(exception.what(),  "Test");
+}
